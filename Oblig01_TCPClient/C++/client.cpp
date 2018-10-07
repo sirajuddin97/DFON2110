114d35a08cc2751c794109194825cb0a7563a0b7
Sirajuddin Asjad <sirasjad@gmail.com>
2018-10-07 04:19:16 +0200

    Fresh code

--- Oblig01_TCPClient/C++/client.cpp
+++ Oblig01_TCPClient/C++/client.cpp
@@ -16,47 +16,29 @@ void Socket::connectSocket(){
 }
 
 void Socket::sendData(){
-	const int msgSize = 10;
-	std::string student_nr;
+	char* studnr = new char[6];
 	std::cout << "\nSkriv inn student nummeret ditt: ";
-	std::cin >> student_nr;
-	if(strlen(student_nr.c_str()) != 6) throw INVALID_STUDNR;
-	
+	std::cin >> studnr;
+	if(strlen(studnr) != 6) throw INVALID_STUDNR;
+
+	char msg_id = 0x01;
+	char studnr_size = strlen(studnr);
+	short msg_size = sizeof(msg_id) + strlen(studnr); // Big endian?? 
+
 	memset(buffer, 0, bufferSize);
-	//char test = 0x01;
-	//short test2 = 7;
-	//char msg = 6;
-	
-	buffer[0] = 0x01; // Melding ID
-	buffer[1] = 0; // Size of message (Big endian)
-	buffer[2] = 7; // Size of message (Big endian)
-	buffer[3] = 6; // Size of studentnr
-	//buffer[4] = '2';
-	//buffer[5] = '1';
-	//buffer[6] = '6';
-	//buffer[7] = '9';
-	//buffer[8] = '8';
-	//buffer[9] = '8';
+	memcpy(buffer, &msg_id, sizeof(msg_id));
+	memcpy(buffer+1, &msg_size, sizeof(msg_size));
+	memcpy(buffer+3, &studnr_size, sizeof(studnr_size));
+	memcpy(buffer+4, studnr, strlen(studnr));
 
-	//memcpy(buffer, &test, sizeof(test));
-	//memcpy(buffer+2, &test2, sizeof(test2));
-	//memcpy(buffer+3, &msg, sizeof(msg));
-
-	std::string studentID = "216988";
-
-//	char* studentID = "216988";
-    memcpy(buffer + 4, stu, 6);
-
-	//memcpy(buffer+4, &student_nr, strlen(student_nr.c_str()));
-
-	int s = send(sock, buffer, msgSize, 0);
-	//int s = send(sock, student_nr.c_str(), strlen(student_nr.c_str()), 0);
+	const int stream_size = sizeof(msg_id) + sizeof(msg_size) + sizeof(studnr_size) + strlen(studnr);
+	int s = send(sock, buffer, stream_size, 0);
 	if(s < 0) throw SEND_FAIL;
 }
 
 void Socket::receiveData(){
-	memset(buffer, 0, 1024);
-	int r = recv(sock, buffer, sizeof(buffer), 0);
+	memset(buffer, 0, bufferSize);
+	int r = recv(sock, buffer, bufferSize, 0);
 	if(r < 0) throw READ_FAIL;
 	std::cout << buffer << std::endl;
 }
