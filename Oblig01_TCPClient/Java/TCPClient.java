3aa7095041d68d666f46c39ed488d65aa3676dca
Sirajuddin Asjad <sirasjad@gmail.com>
2018-10-07 17:28:02 +0200

    Update

---
+++ Oblig01_TCPClient/Java/TCPClient.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+ 
+public class TCPClient{
+ 
+    private static Socket socket;
+ 
+    public static void main(String args[])
+    {
+        try
+        {
+            String host = "158.36.70.56";
+            int port = 80;
+            InetAddress address = InetAddress.getByName(host);
+            socket = new Socket(address, port);
+ 
+            //Send the message to the server
+            OutputStream os = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+ 
+            String number = "176216988";
+ 
+            String sendMessage = number + "\n";
+            bw.write(sendMessage);
+            bw.flush();
+            System.out.println("Message sent to the server : "+sendMessage);
+ 
+            //Get the return message from the server
+            InputStream is = socket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String message = br.readLine();
+            System.out.println("Message received from the server : " +message);
+        }
+        catch (Exception exception)
+        {
+            exception.printStackTrace();
+        }
+        finally
+        {
+            //Closing the socket
+            try
+            {
+                socket.close();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
