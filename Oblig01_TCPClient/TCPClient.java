1af663c16a5c670ba704592e4acd791c14f8c95a
Sirajuddin Asjad <sirasjad@gmail.com>
2018-10-10 20:52:16 +0200

    Quick fix

--- Oblig01_TCPClient/TCPClient.java
+++ Oblig01_TCPClient/TCPClient.java
@@ -1,5 +1,11 @@
-import java.io.*;
-import java.net.*;
+//import java.io.*;
+//import java.net.*;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.InetAddress;
 import java.util.Scanner;
 
 public class TCPClient{
