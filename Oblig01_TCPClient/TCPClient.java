5e1d6e9d97e6c820aa03ce75def626f19ed31d58
Sirajuddin Asjad <sirasjad@gmail.com>
2018-10-11 00:22:12 +0200

    Fix

--- Oblig01_TCPClient/TCPClient.java
+++ Oblig01_TCPClient/TCPClient.java
@@ -1,5 +1,3 @@
-//import java.io.*;
-//import java.net.*;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -47,7 +45,7 @@ public class TCPClient{
 
 	private void sendData() throws Exception{
 		char msg_id = 0x01;
-		short msg_size = 7; // kan jeg droppe short?
+		short msg_size = 7;
 		char studnr_size = 6;
 
 		bufferOut[0] = msg_id; // Message ID
